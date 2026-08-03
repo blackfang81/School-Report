40b60e55945d632500666afb85d6395bfe6a31ba
AmirHossein Kheirash <amirhossein.kheirash@gmail.com>
2026-08-04 03:05:12 +0330

    Update model accounts - Role

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,12 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+class Role(models.TextChoices):
+    TEACHER = "teacher", "Teacher"
+    EDUCATION_OFFICER = "education_officer", "Education Officer"
+    FINANCE_OFFICER = "finance_officer", "Finance Officer"
+
+
 class User(AbstractUser):
     role = models.CharField(max_length=30, choices=Role.choices)
     phone = models.CharField(max_length=20, blank=True)
