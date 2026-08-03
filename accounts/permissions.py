5f10fc21bd80d06189463d44e24ee6b88b30a7c2
AmirHossein Kheirash <amirhossein.kheirash@gmail.com>
2026-08-04 03:07:50 +0330

    Update permissions -  IsEducationOfficer

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -5,6 +5,10 @@ class IsTeacher(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.is_teacher
 
+class IsEducationOfficer(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.is_education_officer
+
 class IsFinanceOfficer(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.is_finance_officer        
