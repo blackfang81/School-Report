451a21239401208e2742df014377e5169f12da39
AmirHossein Kheirash <amirhossein.kheirash@gmail.com>
2026-08-04 03:22:17 +0330

    Add model education - School

--- education/models.py
+++ education/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class School(models.Model):
+    name = models.CharField(max_length=200)
+    level = models.CharField(max_length=50, blank=True)
+    gender = models.CharField(max_length=20, blank=True)
+    email = models.EmailField(blank=True)
+    phone = models.CharField(max_length=20, blank=True)
+    fax = models.CharField(max_length=20, blank=True)
+    address = models.TextField(blank=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
