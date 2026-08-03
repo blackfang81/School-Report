15ec6a4b36a9dc39d82c92c8565dc7980a894c3d
AmirHossein Kheirash <amirhossein.kheirash@gmail.com>
2026-08-04 03:12:10 +0330

    Add admin.py to accounts

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ("username", "first_name", "last_name", "role", "phone", "is_active")
+    list_filter = ("role", "is_active")
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ("Extra", {"fields": ("role", "phone", "emergency_phone")}),
+    )
+    add_fieldsets = BaseUserAdmin.add_fieldsets + (
+        ("Extra", {"fields": ("role", "phone", "emergency_phone")}),
+    )
