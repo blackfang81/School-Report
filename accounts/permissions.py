d3fc4cd71155c23d74e7c1f30d8a876fdbdd53b8
AmirHossein Kheirash <amirhossein.kheirash@gmail.com>
2026-08-04 03:07:26 +0330

    Update permissions -  IsFinance

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -4,3 +4,7 @@ from rest_framework import permissions
 class IsTeacher(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.is_teacher
+
+class IsFinanceOfficer(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.is_finance_officer        
