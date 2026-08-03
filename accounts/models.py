710857e07ac6097c625276867a018ae6645e125f
AmirHossein Kheirash <amirhossein.kheirash@gmail.com>
2026-08-04 03:04:28 +0330

    Add model accounts

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    role = models.CharField(max_length=30, choices=Role.choices)
+    phone = models.CharField(max_length=20, blank=True)
+    emergency_phone = models.CharField(max_length=20, blank=True)
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+    def __str__(self):
+        return f"{self.get_full_name() or self.username} ({self.get_role_display()})"
+
+    @property
+    def is_teacher(self):
+        return self.role == Role.TEACHER
+
+    @property
+    def is_education_officer(self):
+        return self.role == Role.EDUCATION_OFFICER
+
+    @property
+    def is_finance_officer(self):
+        return self.role == Role.FINANCE_OFFICER
