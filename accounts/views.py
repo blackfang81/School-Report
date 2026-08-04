1f69e88fe98c412f67d3db451fbf4e5371d950b7
AmirHossein Kheirash <amirhossein.kheirash@gmail.com>
2026-08-04 03:37:03 +0330

    Add accounts views - Meview

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+from .models import Role, User
+from .permissions import IsEducationOfficer
+from .serializers import ChangePasswordSerializer, ProfileUpdateSerializer, UserSerializer
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    """Login with username and password; returns JWT tokens."""
+
+
+class MeView(generics.RetrieveUpdateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
+
+    def get_serializer_class(self):
+        if self.request.method in ("PUT", "PATCH"):
+            return ProfileUpdateSerializer
+        return UserSerializer
+
+    def update(self, request, *args, **kwargs):
+        serializer = ProfileUpdateSerializer(self.get_object(), data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(UserSerializer(self.get_object()).data)
+
