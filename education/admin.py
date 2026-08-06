cb74f9dc361c39d83b4e627b357464d93c3af7c4
AmirHossein Kheirash <amirhossein.kheirash@gmail.com>
2026-08-06 15:26:06 +0330

    update education admin.py

--- education/admin.py
+++ education/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ClassRoom, School, TeacherAssignment, Term
+
+
+@admin.register(School)
+class SchoolAdmin(admin.ModelAdmin):
+    list_display = ("name", "level", "phone")
+
+
+@admin.register(Term)
+class TermAdmin(admin.ModelAdmin):
+    list_display = ("name", "start_date", "end_date", "is_summer")
+
+
+@admin.register(ClassRoom)
+class ClassRoomAdmin(admin.ModelAdmin):
+    list_display = ("name", "school", "term", "session_duration")
+
+
+@admin.register(TeacherAssignment)
+class TeacherAssignmentAdmin(admin.ModelAdmin):
+    list_display = ("classroom", "teacher", "start_date", "end_date")
