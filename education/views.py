7c6fad0d7e137d96591a22188333acd9f982b51e
AmirHossein Kheirash <amirhossein.kheirash@gmail.com>
2026-08-06 15:27:07 +0330

    Add education views - SchoolViewSet

--- education/views.py
+++ education/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from accounts.permissions import IsEducationOfficer, IsTeacher
+from .models import ClassRoom, School, TeacherAssignment, Term
+from .serializers import (
+    ClassRoomSerializer,
+    SchoolSerializer,
+    TeacherAssignmentSerializer,
+    TermSerializer,
+)
+
+
+class SchoolViewSet(viewsets.ModelViewSet):
+    queryset = School.objects.all()
+    serializer_class = SchoolSerializer
+    filterset_fields = ["name", "level", "gender"]
+    search_fields = ["name"]
+    ordering_fields = ["name"]
+
+    def get_permissions(self):
+        if self.action in ("list", "retrieve"):
+            return [IsAuthenticated()]
+        return [IsEducationOfficer()]
