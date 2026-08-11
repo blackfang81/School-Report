84aee776856b809398b1655b877e2ca71df3120b
AmirHossein Kheirash <amirhossein.kheirash@gmail.com>
2026-08-12 02:33:31 +0330

    Add 'is_deleted' field to admin education

--- education/admin.py
+++ education/admin.py
@@ -5,19 +5,23 @@ from .models import ClassRoom, School, TeacherAssignment, Term
 
 @admin.register(School)
 class SchoolAdmin(admin.ModelAdmin):
-    list_display = ("name", "level", "phone")
+    list_display = ("name", "level", "phone", "is_deleted")
+    list_filter = ("is_deleted",)
 
 
 @admin.register(Term)
 class TermAdmin(admin.ModelAdmin):
-    list_display = ("name", "start_date", "end_date", "is_summer")
+    list_display = ("name", "start_date", "end_date", "is_summer", "is_deleted")
+    list_filter = ("is_summer", "is_deleted")
 
 
 @admin.register(ClassRoom)
 class ClassRoomAdmin(admin.ModelAdmin):
-    list_display = ("name", "school", "term", "session_duration")
+    list_display = ("name", "school", "term", "session_duration", "is_deleted")
+    list_filter = ("session_duration", "is_deleted")
 
 
 @admin.register(TeacherAssignment)
 class TeacherAssignmentAdmin(admin.ModelAdmin):
-    list_display = ("classroom", "teacher", "start_date", "end_date")
+    list_display = ("classroom", "teacher", "start_date", "end_date", "is_deleted")
+    list_filter = ("is_deleted",)
